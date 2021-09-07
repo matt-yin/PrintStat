b1dddb50d8d7bb7654cc31bf81155145c5cc1e1f
Matt Yin@ <matt.yin@icloud.com>
2021-09-07 15:01:28 -0400

    Load Job Verified

--- Program.cs
+++ Program.cs
@@ -12,12 +12,12 @@ namespace PrintStat
 
             // for every job in the list, get the create date and parse the case number
 
-            //string PrintingLogDir = @"E:\MattWorkspace\PrintStat\4. Printing Log\2021";
+            string PrintingLogDir = @"Z:\3. Patients for Printing\4. Printing Log\2021";
 
             Statistics stat = new Statistics();
-            // stat.LoadJobFiles(PrintingLogDir);
-            // stat.GetCaseInfo();
-            stat.CreateDataTable();
+            stat.LoadJobFiles(PrintingLogDir);
+            stat.GetCaseInfo();
+            //stat.CreateDataTable();
 
         }
     }
